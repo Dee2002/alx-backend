c3b6ce8a92ef99cdcb27406d701d56d2bbca59f4
Delsa Marasha <Deemarasha77@gmail.com>
2024-05-20 21:35:35 +0000

    3. Deletion-resilient hypermedia pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -4,23 +4,12 @@ Deletion-resilient hypermedia pagination
 """
 
 import csv
-import math
-from typing import List, Dict
-
-
-def index_range(page: int, page_size: int) -> tuple:
-    """
-    Return a tuple of size two containing a start index and an end index
-    corresponding to the range of indexes to return in a list for those
-    particular pagination parameters.
-    """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
+from typing import List, Dict, Any
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -28,44 +17,50 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[1:]  # Exclude header row
+            self.__dataset = dataset[1:]
 
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0"""
+        """Dataset indexed by sorting position, starting at 0
+        """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
-            self.__indexed_dataset = {i: dataset[i] for i in
-                                      range(len(dataset))}
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """Return a dictionary with pagination information resilient
-        to deletions"""
-        assert isinstance(index, int) and index >= 0
-        assert isinstance(page_size, int) and page_size > 0
+    def get_hyper_index(
+        self, index: int = None, page_size: int = 10
+    ) -> Dict[str, Any]:
+        """Deletion-resilient hypermedia pagination
+        """
+        assert isinstance(index, int) and isinstance(page_size, int)
+        assert 0 <= index < len(self.indexed_dataset())
+        assert page_size > 0
 
-        indexed_dataset = self.indexed_dataset()
+        dataset = self.indexed_dataset()
         data = []
         next_index = index
 
-        for i in range(page_size):
-            while next_index not in indexed_dataset and
-            next_index < len(self.dataset()):
+        for _ in range(page_size):
+            while next_index not in dataset and next_index < len(dataset):
                 next_index += 1
-            if next_index in indexed_dataset:
-                data.append(indexed_dataset[next_index])
-                next_index += 1
+            if next_index >= len(dataset):
+                break
+            data.append(dataset[next_index])
+            next_index += 1
 
         return {
-            'index': index,
-            'next_index': next_index,
-            'page_size': len(data),
-            'data': data
+            "index": index,
+            "next_index": next_index,
+            "page_size": len(data),
+            "data": data
         }
