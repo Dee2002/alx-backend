6d35eacf8ca212698002e08c8cf87ca2107a6ee3
Delsa Marasha <Deemarasha77@gmail.com>
2024-05-22 16:11:15 +0000

    Fix FIFOCache and LIFOCache implementations to pass all test cases.

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
-""" FIFOCache module """
+""" 1-fifo_cache.py """
 
 from base_caching import BaseCaching
-from collections import OrderedDict
 
 
 class FIFOCache(BaseCaching):
-    """ FIFOCache defines a FIFO caching system """
+    """ FIFO caching system """
 
     def __init__(self):
-        """ Initialize the cache """
+        """ Initialize """
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.order = []
 
     def put(self, key, item):
         """ Add an item in the cache """
-        if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS and
-            key not in self.cache_data:
-                first_key = next(iter(self.cache_data))
-                print("DISCARD:", first_key)
-                self.cache_data.pop(first_key)
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
+
+        if key not in self.cache_data:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                first_key = self.order.pop(0)
+                del self.cache_data[first_key]
+                print(f"DISCARD: {first_key}")
+
+        self.cache_data[key] = item
+        if key not in self.order:
+            self.order.append(key)
 
     def get(self, key):
         """ Get an item by key """
--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
-""" LIFOCache module """
+""" 2-lifo_cache.py """
 
 from base_caching import BaseCaching
 
-
 class LIFOCache(BaseCaching):
-    """ LIFOCache defines a LIFO caching system """
+    """ LIFO caching system """
 
     def __init__(self):
-        """ Initialize the cache """
+        """ Initialize """
         super().__init__()
+        self.last_key = None
 
     def put(self, key, item):
         """ Add an item in the cache """
-        if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS and
-            key not in self.cache_data:
-                last_key = list(self.cache_data.keys())[-1]
-                print("DISCARD:", last_key)
-                del self.cache_data[last_key]
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= self.MAX_ITEMS and key not in self.cache_data:
+            if self.last_key is not None:
+                del self.cache_data[self.last_key]
+                print(f"DISCARD: {self.last_key}")
+
+        self.cache_data[key] = item
+        self.last_key = key
 
     def get(self, key):
         """ Get an item by key """
